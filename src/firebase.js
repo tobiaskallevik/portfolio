1369e1148e12638ff7c2e89cecb07bb8c9695d78
Tobias Fidje Kallevik <tobiaskallevik@gmail.com>
2025-08-20 21:52:55 +0200

    Updated portfolio data

--- src/firebase.js
+++ src/firebase.js
@@ -14,11 +14,11 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+
 export const database = getDatabase(app);
